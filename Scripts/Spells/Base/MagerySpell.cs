da5bcbeb85465d334897f3a14d75b269728c0d4e
Emir Ozer <emirozer@yandex.com>
2016-01-05 16:13:51 +0100

    normally the spell delays increase a quarter per circle, increase the delays thsi time to half , because its too fast

--- Scripts/Spells/Base/MagerySpell.cs
+++ Scripts/Spells/Base/MagerySpell.cs
@@ -131,7 +131,7 @@ namespace Server.Spells
 				return TimeSpan.Zero;
 
 			if( !Core.AOS )
-				return TimeSpan.FromSeconds( 0.5 + (0.25 * (int)Circle) );
+				return TimeSpan.FromSeconds( 0.5 + (0.50 * (int)Circle) );
 
 			return base.GetCastDelay();
 		}
