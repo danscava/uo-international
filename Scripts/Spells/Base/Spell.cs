5520034a1d35396aec608aed5672e291f387b9e0
Emir Ozer <emirozer@yandex.com>
2016-01-08 14:32:46 +0100

    only champions cannot be disturbed

--- Scripts/Spells/Base/Spell.cs
+++ Scripts/Spells/Base/Spell.cs
@@ -163,10 +163,15 @@ namespace Server.Spells
 
 		public virtual void OnCasterHurt()
 		{
+			// only champions cannot be disturbed.
+			if (!Caster.Player && (Caster.Name == "Barracoon" || Caster.Name == "the harrower" || Caster.Name == "Lord Oaks" || Caster.Name == "Mephitis" || Caster.Name == "Neira"|| Caster.Name == "Rikktor" || Caster.Name == "Serado"|| Caster.Name == "Semidar" )){
+				return;
+			}
 			//Confirm: Monsters and pets cannot be disturbed.
 			//if ( !Caster.Player )
 			//	return;
 			// 07.01.2016 (EMIR) THEY SHOULD BE ABLE TO DISTURBED
+
 
 			if ( IsCasting )
 			{
